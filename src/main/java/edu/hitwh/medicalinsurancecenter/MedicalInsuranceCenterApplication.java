17300c9ed8c2a32885edbf38b5fbe89789419e52
wxy3265 <546668033@qq.com>
2024-07-22 14:18:08 +0800

    Initial commit

---
+++ src/main/java/edu/hitwh/medicalinsurancecenter/MedicalInsuranceCenterApplication.java
@@ -0,0 +1,13 @@
+package edu.hitwh.medicalinsurancecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalInsuranceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalInsuranceCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/hitwh/medicalinsurancecenter/MedicalInsuranceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hitwh.medicalinsurancecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalInsuranceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
