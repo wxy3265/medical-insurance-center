809eca46c0ef22cbd0e008f27b379c6945f7088a
wxy3265 <546668033@qq.com>
2024-07-26 10:55:49 +0800

    修复了配置消息转换器后日期格式不正确的问题

--- src/main/java/edu/hitwh/medicalinsurancecenter/config/WebMvcConfiguration.java
+++ src/main/java/edu/hitwh/medicalinsurancecenter/config/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package edu.hitwh.medicalinsurancecenter.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -17,13 +18,14 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        converters.removeIf( x -> x instanceof MappingJackson2HttpMessageConverter);
+        converters.removeIf( x -> x instanceof MappingJackson2HttpMessageConverter );
         MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
         ObjectMapper objectMapper = new ObjectMapper();
         SimpleModule simpleModule = new SimpleModule();
         simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
         simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
         objectMapper.registerModule(simpleModule);
+        objectMapper.registerModule(new JavaTimeModule());
         jackson2HttpMessageConverter.setObjectMapper(objectMapper);
         converters.add(jackson2HttpMessageConverter);
     }
